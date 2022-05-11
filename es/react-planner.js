0b4feb3371849a72f13432d23338e2232308ccfc
Enes K <ekolcak1@gmail.com>
2022-05-11 16:52:49 +0300

    ll

--- es/react-planner.js
+++ es/react-planner.js
@@ -113,11 +113,6 @@ var ReactPlanner = function (_Component) {
       return React.createElement(
         "div",
         { style: _extends({}, wrapperStyle, { height: height }) },
-        React.createElement(Toolbar, _extends({
-          width: toolbarW,
-          height: toolbarH,
-          state: extractedState
-        }, props)),
         React.createElement(Content, _extends({
           width: contentW,
           height: contentH,
